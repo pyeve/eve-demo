affd725b802b7036104a074961642cda5deee622
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-09-03 17:26:20 +0200

    Pick appropriate application port and host.

--- run.py
+++ run.py
@@ -20,7 +20,12 @@ from eve import Eve
 
 if __name__ == '__main__':
     # Heroku support: bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    if 'PORT' in os.environ:
+        port = int(os.environ.get('PORT'))
+        host = '0.0.0.0'
+    else:
+        port = 5000
+        host = '127.0.0.1'
 
     app = Eve()
-    app.run(host='0.0.0.0', port=port)
+    app.run(host=host, port=port)
