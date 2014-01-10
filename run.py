8ccdd028347a73fc4706b0d34c4ee90eb9777336
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-01-10 21:16:20 +0100

    Use '0.0.0.0' to ensure your API is reachable outside localhost.

--- run.py
+++ run.py
@@ -22,6 +22,8 @@ if __name__ == '__main__':
     # Heroku support: bind to PORT if defined, otherwise default to 5000.
     if 'PORT' in os.environ:
         port = int(os.environ.get('PORT'))
+        # use '0.0.0.0' to ensure your REST API is reachable from all your
+        # network (and not only your computer).
         host = '0.0.0.0'
     else:
         port = 5000
