743322ad208b18092821314fe3938bebefe77972
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-05-20 13:41:28 +0200

    Document owner embeddeing

--- settings.py
+++ settings.py
@@ -134,8 +134,9 @@ works = {
             # will default to `people._id` (or, more precisely, to whatever
             # ID_FIELD value is).
             'data_relation': {
-                'embeddable': True,
-                'resource': 'people'
+                'resource': 'people',
+                # make the owner embeddable with ?embedded={"owner":1}
+                'embeddable': True
             },
         },
     }
