2628dd6fc4e60a1cfe6cec4d0a1a63a320298713
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-01-12 17:29:16 +0100

    Je Suis Charlie

--- run.py
+++ run.py
@@ -18,16 +18,24 @@
 import os
 from eve import Eve
 
-if __name__ == '__main__':
-    # Heroku support: bind to PORT if defined, otherwise default to 5000.
-    if 'PORT' in os.environ:
-        port = int(os.environ.get('PORT'))
-        # use '0.0.0.0' to ensure your REST API is reachable from all your
-        # network (and not only your computer).
-        host = '0.0.0.0'
-    else:
-        port = 5000
-        host = '127.0.0.1'
+# Heroku support: bind to PORT if defined, otherwise default to 5000.
+if 'PORT' in os.environ:
+    port = int(os.environ.get('PORT'))
+    # use '0.0.0.0' to ensure your REST API is reachable from all your
+    # network (and not only your computer).
+    host = '0.0.0.0'
+else:
+    port = 5000
+    host = '127.0.0.1'
 
-    app = Eve()
+app = Eve()
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('X-Ahmed', 'Je Suis Ahmed.')
+    response.headers.add('X-Charlie', 'Je Suis Charlie.')
+    return response
+
+if __name__ == '__main__':
     app.run(host=host, port=port)
